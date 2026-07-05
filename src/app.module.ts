2c76fb4f7be58eb7635784cc0630dc620455ba8f
7thDragon <slava.lepeshkin01@gmail.com>
2026-07-06 00:32:06 +0600

    feat: add typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: process.env.POSTGRES_PORT
+        ? parseInt(process.env.POSTGRES_PORT)
+        : 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
