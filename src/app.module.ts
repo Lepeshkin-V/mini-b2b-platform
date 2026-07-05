2357bb1ce63d5f80ee6c1ab019a649d1b12c803a
7thDragon <slava.lepeshkin01@gmail.com>
2026-07-06 00:39:55 +0600

    fix: use fixed port

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgres',
-      port: process.env.POSTGRES_PORT
-        ? parseInt(process.env.POSTGRES_PORT)
-        : 5432,
+      port: 5432,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
