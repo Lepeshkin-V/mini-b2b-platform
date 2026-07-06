a3b991714d7801087a3c035809c977aaaf9fbdb6
7thDragon <slava.lepeshkin01@gmail.com>
2026-07-06 23:38:55 +0600

    refactor: use typeorm async import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import TypeOrmConfigService from './common/services/typeorm-config.service';
+import configuration, {
+  validationOptions,
+  validationSchema,
+} from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
+      validationSchema,
+      validationOptions,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [],
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
     }),
   ],
   controllers: [AppController],
